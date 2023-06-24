e1d463a79b7baf4981e040bdabc9d9ae9fb7a48d
FudgyDRS <yetimonsters@gmail.com>
2023-06-24 17:56:56 -0400

    add escrow, payment, and history structs

--- contracts/contracts/AccountEscrow.sol
+++ contracts/contracts/AccountEscrow.sol
@@ -2,6 +2,41 @@
 pragma solidity ^0.8.12;
 
 contract AccountEscrow {
+/** This escrow contract (Account Escrow) is a singleton contract
+  * The purpose of this contract is to be the intermediary contract
+  * for storing account funds (tokens and ETH) to bribe paymasters 
+  * within the 4337 ecosystem to pay for autonomously executing
+  * transactions on any EVM chain
+  *
+  * To support this functionality this contract must
+  *     - map account to funds
+  *     - escrow must have a freeze period
+  *     - payback amount and rceipient must be deterministic
+  *     - must have a handle function to accept messages from hyperlane mailbox
+  *
+  * Simplest solution to payback
+  * userop calldata has target address, original transactions data, and signature
+  * to reconstruct the signer (ie signer == target)
+  */
 
-    
+
+  mapping(address => Escrow) accountInfo;
+
+  struct Escrow {
+    uint256 freezeStart;
+    uint256 freezeStop;
+    uint256 nonce;
+    mapping(address => uint256) balance;
+    mapping(uint256 => Payment) history;
+  }
+
+  struct Payment {
+    uint256 timestamp;
+    uint256 assetAmount;
+    bytes32 domainId;
+    address from;
+    address asset;
+  }
+
+
 }
