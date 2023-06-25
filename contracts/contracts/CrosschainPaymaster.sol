e78330a03a5ad14ec30e8aee3b58a71be66f7cb3
FudgyDRS <yetimonsters@gmail.com>
2023-06-24 22:09:33 -0400

    added IInterchainGasPaymaster

--- contracts/contracts/CrosschainPaymaster.sol
+++ contracts/contracts/CrosschainPaymaster.sol
@@ -6,6 +6,56 @@ pragma solidity ^0.8.12;
 import "./aa/core/EntryPoint.sol";
 import "./aa/core/BasePaymaster.sol";
 
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.6.11;
+
+/**
+ * @title IInterchainGasPaymaster
+ * @notice Manages payments on a source chain to cover gas costs of relaying
+ * messages to destination chains.
+ */
+interface IInterchainGasPaymaster {
+    /**
+     * @notice Emitted when a payment is made for a message's gas costs.
+     * @param messageId The ID of the message to pay for.
+     * @param gasAmount The amount of destination gas paid for.
+     * @param payment The amount of native tokens paid.
+     */
+    event GasPayment(
+        bytes32 indexed messageId,
+        uint256 gasAmount,
+        uint256 payment
+    );
+
+    /**
+     * @notice Deposits msg.value as a payment for the relaying of a message
+     * to its destination chain.
+     * @dev Overpayment will result in a refund of native tokens to the _refundAddress.
+     * Callers should be aware that this may present reentrancy issues.
+     * @param _messageId The ID of the message to pay for.
+     * @param _destinationDomain The domain of the message's destination chain.
+     * @param _gasAmount The amount of destination gas to pay for.
+     * @param _refundAddress The address to refund any overpayment to.
+     */
+    function payForGas(
+        bytes32 _messageId,
+        uint32 _destinationDomain,
+        uint256 _gasAmount,
+        address _refundAddress
+    ) external payable;
+
+    /**
+     * @notice Quotes the amount of native tokens to pay for interchain gas.
+     * @param _destinationDomain The domain of the message's destination chain.
+     * @param _gasAmount The amount of destination gas to pay for.
+     * @return The amount of native tokens required to pay for interchain gas.
+     */
+    function quoteGasPayment(uint32 _destinationDomain, uint256 _gasAmount)
+        external
+        view
+        returns (uint256);
+}
+
 /// @title ross-chain Paymaster for ERC-4337
 /// @notice Paymaster to send crosschain payments
 /// @dev Inherits from BasePaymaster.
@@ -105,7 +155,8 @@ contract CrosschainPaymaster is BasePaymaster {
             dummyData, // signature
             userOp.callData
             );
-        IMailbox(ethereumMailbox).dispatch(goerliDomain, addressToBytes32(accountEscrow), bribeRequest);
+        bytes32 messageId = IMailbox(ethereumMailbox).dispatch(goerliDomain, addressToBytes32(accountEscrow), bribeRequest);
+        
     }}
 
     /// @notice Performs post-operation tasks,
